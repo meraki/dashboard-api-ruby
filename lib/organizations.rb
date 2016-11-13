f78e6e23d3d71ff3edf294fe7045066eafba17ec
Joe Letizia <letiziajm@gmail.com>
2016-11-13 10:24:51 -0800

    Added POST functionality (details below)
    dashboard_api:
    Now contains support for POST functionality. The actual options that will be passed during the post, such as
    the network name and type when creating a new network are part of the 'body' section and automatically gets
    serialized to json.
    
    The POST will also watch out for errors, such as a network already existing, and will raise an exception for this
    letting you know why it errored.
    
    test_dashboard_api:
    3 new tests. 1 to check basic functionality of the make_api_call for GET
    1 to check the basic functionality of make_api_call for POST
    1 to check the basic error handling of make_api_call for POST
    
    organizations.rb
    Since make_api_call now requries an HTTP_METHOD to be passed along side of it, all previous calls needed
    to be updated appropriately

--- lib/organizations.rb
+++ lib/organizations.rb
@@ -5,28 +5,28 @@ module Organizations
   # @param [String] org_id dashboard organization ID
   # @return [Hash] results contains the org  id and name of the given organization
   def get_organization(org_id)
-    self.make_api_call("/organizations/#{org_id}")
+    self.make_api_call("/organizations/#{org_id}", 'GET')
   end
 
   # Returns the current license state for a given organization
   # @param [String] org_id dashboard organization ID
   # @return [Hash] results contains the current license state information
   def get_license_state(org_id)
-    self.make_api_call("/organizations/#{org_id}/licenseState")
+    self.make_api_call("/organizations/#{org_id}/licenseState", 'GET')
   end
 
   # Returns the current inventory for an organization
   # @param [String] org_id dashboard organization ID
   # @return [Array] an array of hashes containg information on each individual device
   def get_inventory(org_id)
-    self.make_api_call("/organizations/#{org_id}/inventory")
+    self.make_api_call("/organizations/#{org_id}/inventory", 'GET')
   end
 
   # Returns the current SNMP status for an organization
   # @param [String] org_id dashboard organization ID
   # @return [Hash] a hash containing all SNMP configuration information for an organization
   def get_snmp_status(org_id)
-    self.make_api_call("/organizations/#{org_id}/snmp")
+    self.make_api_call("/organizations/#{org_id}/snmp", 'GET')
   end
 
   # Returns the configurations for an organizations 3rd party VPN peers
@@ -34,6 +34,6 @@ module Organizations
   # @return [Array] an arrry of hashes containing the configuration information
   #   for each 3rd party VPN peer
   def get_third_party_peers(org_id)
-    self.make_api_call("/organizations/#{org_id}/thirdPartyVPNPeers")
+    self.make_api_call("/organizations/#{org_id}/thirdPartyVPNPeers", 'GET')
   end
 end
